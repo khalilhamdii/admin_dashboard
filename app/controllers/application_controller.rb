f7ea5dc6500195a57a13a6a3752b1463340d35b8
khalilhamdii <khaalil.hamdi@gmail.com>
2021-03-26 21:52:34 +0100

    Add styling to admin portal page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Access denied."
+    redirect_to root_url
+end
   def after_sign_in_path_for(resource)
     if current_user.admin_role?
      portal_path
