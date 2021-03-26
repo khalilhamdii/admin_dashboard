677f8377cd0b96029c99cb3886dfdd30bc3ac404
khalilhamdii <khaalil.hamdi@gmail.com>
2021-03-26 16:32:27 +0100

    Add portal and dashboard routes + authentification restriction

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "admin_portal#index"
+  root to: "user_dashboard#index"
+  get '/portal', to: 'admin_portal#index', as: 'portal'
+  get '/dashboard', to: 'user_dashboard#index', as: 'dashboard'
 end
