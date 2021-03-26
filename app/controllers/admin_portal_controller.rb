6a74e9a9d6b952072201f63b8f4252a5e5e2d586
khalilhamdii <khaalil.hamdi@gmail.com>
2021-03-27 00:06:33 +0100

    Add new user form + set routes + add controller methods to admin portal

--- app/controllers/admin_portal_controller.rb
+++ app/controllers/admin_portal_controller.rb
@@ -1,4 +1,70 @@
 class AdminPortalController < ApplicationController
-  def index 
+  before_action :authenticate_user!, :redirect_unless_admin
+  before_action :set_user, only: %i[ edit update destroy ]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  # # GET /users/1/edit
+  # def edit
+  # end
+
+  # # POST /users or /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to portal_path, notice: "User account successfully created. A password reset email has been sent to the user." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # # PATCH/PUT /users/1 or /users/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @user.update(user_params)
+  #       format.html { redirect_to @user, notice: "User was successfully updated." }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # # DELETE /users/1 or /users/1.json
+  # def destroy
+  #   @user.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+  #   end
+  # end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:full_name, :email, :phone_number, :company_name, :company_address)
+  end
+
+  def redirect_unless_admin
+    unless current_user.admin_role?
+      flash[:error] = "Access Denied!"
+      redirect_to root_path
+    end
+  end
+
+  def sign_up(resource_name, resource)
+    true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new"
+  end
+  devise_for :users, :skip => [:registrations] 
+
   root to: "user_dashboard#index"
   get '/portal', to: 'admin_portal#index', as: 'portal'
+  get "/portal/new" => "admin_portal#new", as: "new_user_registration"
+  post "/portal/new" => "admin_portal#create", as: "user_registration"
   get '/dashboard', to: 'user_dashboard#index', as: 'dashboard'
 end
