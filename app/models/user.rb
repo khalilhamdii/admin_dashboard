b641147bcdabeda6ea9ced2a3a409ebb94c6711c
khalilhamdii <khaalil.hamdi@gmail.com>
2021-03-27 01:53:55 +0100

    Add feature to send email when creating a new user + configure mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  after_create :send_password_reset_mail
+def send_password_reset_mail
+  UserMailer.send_password_reset(self).deliver_later
+end
 end
